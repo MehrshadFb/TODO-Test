6865bf0136c038b7e881985c7fd1c1a6faafb7d1
Mehrshad <mehrshad.farahbakhsh@gmail.com>
2024-05-10 10:06:33 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,52 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { fetchData } from "./API"
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { fetchData } from "./API";
 
 interface TODO {
-  title: string,
+  title: string;
   id: number;
 }
 
 let nextId = 0;
 
 function App() {
-  const [list, setList] = useState<TODO[]>([])
-  const [task, setTask] = useState<string>('')
-  const [editMode, setEditMode] = useState<boolean>(false)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-
+  const [list, setList] = useState<TODO[]>([]);
+  const [task, setTask] = useState<string>("");
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   // this function handles addding tasks to the list.
   // pre-condition: checks the input is not empty.
   const handleAdd = () => {
-    if (task !== '') {
+    if (task !== "") {
       const newTask: TODO = {
         title: task,
         id: nextId++,
-      }
-      setList([...list, newTask])
-      setTask('')
+      };
+      setList([...list, newTask]);
+      setTask("");
     }
-  }
+  };
 
   // this function handles renaming the title of tasks.
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index: number, key: string) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+    key: string
+  ) => {
     setList((prev) => {
       // check the value of input for edited task is not empty
-      if(e.target.value === "") {
-        window.alert("set a name for your task!")
-      } 
-      else {
-      // make a copy of previous state and save it in copiedList.
-      const copiedList = JSON.parse(JSON.stringify(prev));
-      copiedList[index][key] = e.target.value
-      return copiedList
+      if (e.target.value === "") {
+        window.alert("set a name for your task!");
+      } else {
+        // make a copy of previous state and save it in copiedList.
+        const copiedList = JSON.parse(JSON.stringify(prev));
+        copiedList[index][key] = e.target.value;
+        return copiedList;
       }
-    })
-  }
+    });
+  };
 
   // this function handles deleting tasks from the list.
   const handleDelete = (index: number) => {
@@ -54,53 +55,63 @@ function App() {
       const copiedList = JSON.parse(JSON.stringify(prev));
       // used splice for removing element from a list.
       copiedList.splice(index, 1);
-      return copiedList
-    })
-  }
+      return copiedList;
+    });
+  };
 
   // calling API and fetching data.
   useEffect(() => {
     const Fetch = async () => {
-      const res = await fetchData()
+      const res = await fetchData();
       if (res != null) {
-        setList(res.slice(0,10))
-        setLoading(false)
+        setList(res.slice(0, 10));
+        setLoading(false);
+      } else {
+        setError(true);
+        setLoading(false);
       }
-      else {
-        setError(true)
-        setLoading(false)
-      }
-    }
-    Fetch()
-  },[])
-
-
-  
+    };
+    Fetch();
+  }, []);
 
   return (
     <>
-    <h1>To-Do List</h1>
-    {
-      loading?
+      <h1>To-Do List</h1>
+      {loading ? (
         <p>Loading...</p>
-      :
-        error?
-          <p>Something went wrong!</p>
-        :       
+      ) : error ? (
+        <p>Something went wrong!</p>
+      ) : (
         <div>
-          <input placeholder='task' value={task} onChange={(e) => setTask(e.target.value)}></input>
+          <input
+            placeholder="task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          ></input>
           <button onClick={handleAdd}>Add</button>
-          <button onClick={() =>setEditMode((prev) => !prev)}>Edit</button>
+          <button onClick={() => setEditMode((prev) => !prev)}>Edit</button>
           <ul>
-            {list.map((item, index) => <li key={index}>
-              {editMode ? (<input placeholder='edit' value={item.title} onChange={(e) => handleChange(e, index, 'title')}/>) : (<span>{item.title} <button onClick={()=>handleDelete(index)}>Delete</button></span>)}
-              </li>)}
+            {list.map((item, index) => (
+              <li key={index}>
+                {editMode ? (
+                  <input
+                    placeholder="edit"
+                    value={item.title}
+                    onChange={(e) => handleChange(e, index, "title")}
+                  />
+                ) : (
+                  <span>
+                    {item.title}{" "}
+                    <button onClick={() => handleDelete(index)}>Delete</button>
+                  </span>
+                )}
+              </li>
+            ))}
           </ul>
         </div>
-    }
-    
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tests/components/App.test.tsx
+++ src/tests/components/App.test.tsx
@@ -1,100 +1,98 @@
-import { render, screen, fireEvent } from '@testing-library/react'
-import { vi } from 'vitest'
-import App from '../../App'
+import { render, screen, fireEvent } from "@testing-library/react";
+import { vi } from "vitest";
+import App from "../../App";
 import * as API from "../../API";
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event";
 
+const mockData = [{ title: "task1" }, { title: "task2" }, { title: "task3" }];
+const mockNullData = null;
 
-const mockData = [{title:"task1"}, {title:"task2"}, {title:"task3"}]
-const mockNullData = null
+describe("App", () => {
+  it('should render "To-Do List" heading', () => {
+    render(<App />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/to-do list/i);
+  });
 
+  it('should render "Loading..." while fetching data', () => {
+    render(<App />);
+    const loading = screen.getByText(/loading/i);
+    expect(loading).toBeInTheDocument();
+  });
 
-describe('App', () => {
-    it('should render "To-Do List" heading', () => {
-        render(<App/>)
-        const heading = screen.getByRole("heading")
-        expect(heading).toBeInTheDocument()
-        expect(heading).toHaveTextContent(/to-do list/i)
-    })
+  it("Should fetch API", async () => {
+    const getAPI = vi.spyOn(API, "fetchData");
+    render(<App />);
+    expect(getAPI).toHaveBeenCalledOnce();
+  });
 
-    it('should render "Loading..." while fetching data', () => {
-        render(<App/>)
-        const loading = screen.getByText(/loading/i)
-        expect(loading).toBeInTheDocument()
-    })
-
-    it("Should fetch API", async () => {
-        const getAPI = vi.spyOn(API,'fetchData')
-        render(<App/>);
-        expect(getAPI).toHaveBeenCalledOnce()
+  it("should render tasks if data is avaiable on API", async () => {
+    const fetchSpy = vi.spyOn(API, "fetchData").mockResolvedValue(mockData);
+    render(<App />);
+    expect(fetchSpy).toHaveBeenCalledOnce();
+    mockData.forEach(async (data) => {
+      const task = await screen.findByRole(data.title);
+      expect(task).toBeInTheDocument();
     });
+  });
 
-    it('should render tasks if data is avaiable on API', async() => {
-        const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockData);
-        render(<App/>)
-        expect(fetchSpy).toHaveBeenCalledOnce()
-        mockData.forEach(async(data) => {
-            const task = await screen.findByRole(data.title)
-            expect(task).toBeInTheDocument()
-        })
-    })
+  it("should render error message if data is not avaiable on API", async () => {
+    const fetchSpy = vi.spyOn(API, "fetchData").mockResolvedValue(mockNullData);
+    render(<App />);
+    expect(fetchSpy).toHaveBeenCalledOnce();
+    const error = await screen.findByText(/wrong/i);
+    expect(error).toBeInTheDocument();
+  });
 
-    it('should render error message if data is not avaiable on API', async() => {
-        const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockNullData)
-        render(<App/>)
-        expect(fetchSpy).toHaveBeenCalledOnce()
-        const error = await screen.findByText(/wrong/i)
-        expect(error).toBeInTheDocument()
-    })
+  it("should render tasks and let users delete tasks", async () => {
+    const fetchSpy = vi.spyOn(API, "fetchData").mockResolvedValue(mockData);
+    render(<App />);
+    expect(fetchSpy).toHaveBeenCalledOnce();
+    const btn = await screen.findAllByRole("button", { name: "Delete" });
+    expect(btn).lengthOf(mockData.length);
+    fireEvent.click(btn[0]);
+    expect(screen.queryByText("task1")).not.toBeInTheDocument();
+  });
 
-    it('should render tasks and let users delete tasks', async() => {
-        const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockData);
-        render(<App/>)
-        expect(fetchSpy).toHaveBeenCalledOnce()
-        const btn = await screen.findAllByRole("button", {name:"Delete"})
-        expect(btn).lengthOf(mockData.length)
-        fireEvent.click(btn[0])
-        expect(screen.queryByText("task1")).not.toBeInTheDocument()
-    })
+  it("should render tasks and let users add a new task", async () => {
+    const fetchSpy = vi.spyOn(API, "fetchData").mockResolvedValue(mockData);
+    render(<App />);
+    expect(fetchSpy).toHaveBeenCalledOnce();
+    const button = await screen.findByRole("button", { name: "Add" });
+    const input = screen.getByPlaceholderText(/task/i);
+    const user = userEvent.setup();
+    await user.type(input, "task4");
+    await user.click(button);
+    expect(screen.queryByText("task4")).toBeInTheDocument();
+  });
 
-    it('should render tasks and let users add a new task', async() => {
-        const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockData);
-        render(<App/>)
-        expect(fetchSpy).toHaveBeenCalledOnce()
-        const button = await screen.findByRole("button", {name:"Add"})
-        const input = screen.getByPlaceholderText(/task/i);
-        const user = userEvent.setup()
-        await user.type(input,"task4")
-        await user.click(button)
-        expect(screen.queryByText("task4")).toBeInTheDocument()
-    })
+  it("should render tasks and let users edit tasks", async () => {
+    const fetchSpy = vi.spyOn(API, "fetchData").mockResolvedValue(mockData);
+    render(<App />);
+    expect(fetchSpy).toHaveBeenCalledOnce();
+    const button = await screen.findByRole("button", { name: "Edit" });
+    const user = userEvent.setup();
+    await user.click(button);
+    const input = await screen.findAllByPlaceholderText(/edit/i);
+    await user.clear(input[0]);
+    await user.type(input[0], "edited task1");
+    await user.click(button);
+  });
 
-    it('should render tasks and let users edit tasks', async () => {
-        const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockData);
-        render(<App/>)
-        expect(fetchSpy).toHaveBeenCalledOnce()
-        const button = await screen.findByRole("button", {name:"Edit"})
-        const user = userEvent.setup()
-        await user.click(button)
-        const input = await screen.findAllByPlaceholderText(/edit/i);
-        await user.clear(input[0])
-        await user.type(input[0],"edited task1")
-        await user.click(button)
-    })
+  // it('should render error if users set empty string for edit tasks', async () => {
+  //     const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockData);
+  //     render(<App/>)
+  //     expect(fetchSpy).toHaveBeenCalledOnce()
+  //     const button = await screen.findByRole("button", {name:"Edit"})
+  //     const user = userEvent.setup()
+  //     await user.click(button)
+  //     const input = await screen.findAllByPlaceholderText(/edit/i);
+  //     await user.clear(input[0])
+  //     // const alert = await screen.
+  //     // expect(alert).toBeInTheDocument()
+  //     screen.debug()
 
-    // it('should render error if users set empty string for edit tasks', async () => {
-    //     const fetchSpy = vi.spyOn(API, 'fetchData').mockResolvedValue(mockData);
-    //     render(<App/>)
-    //     expect(fetchSpy).toHaveBeenCalledOnce()
-    //     const button = await screen.findByRole("button", {name:"Edit"})
-    //     const user = userEvent.setup()
-    //     await user.click(button)
-    //     const input = await screen.findAllByPlaceholderText(/edit/i);
-    //     await user.clear(input[0])
-    //     // const alert = await screen.
-    //     // expect(alert).toBeInTheDocument()
-    //     screen.debug()
-
-    //     // const alertMock = vi.spyOn(window, 'alert').mockImplementation();
-    // })
-})
+  //     // const alertMock = vi.spyOn(window, 'alert').mockImplementation();
+  // })
+});
